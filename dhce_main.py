b7a94ce21b865e7b4a0d0b748612b368f111ebbd
Gabe Wasabi <gabewasabi@gmail.com>
2023-11-03 13:31:42 -0700

    modification3

--- dhce_main.py
+++ dhce_main.py
@@ -4,7 +4,7 @@
 # Professor             : Ni Trieu
 # Description           : This program represents a simple DHCE cryptosystem. It takes the values of IV, g, N, x, gy_modN, given_cipherText, given_plainText as command line arguments and prints the decipheredText and cipheredText as output.
 
-# Usage                 : Copy Paste 'Example Usage's Input' on terminal to get the 'Example Usage's Output'
+# Usage                 : Copy Paste "Example Usage's Input" on terminal to get the "Example Usage's Output"
 # General Usage's Input : python dhce_main.py initialization vector, g_e, g_c, N_e, N_c, x, g^y mod N, encrypted message C, plaintext P
 # Example Usage's Input : python dhce_main.py "A2 2D 93 61 7F DC 0D 8E C6 3E A7 74 51 1B 24 B2" 251 465 255 1311 2101864342 8995936589171851885163650660432521853327227178155593274584417851704581358902 "F2 2C 95 FC 6B 98 BE 40 AE AD 9C 07 20 3B B3 9F F8 2F 6D 2D 69 D6 5D 40 0A 75 45 80 45 F2 DE C8 6E C0 FF 33 A4 97 8A AF 4A CD 6E 50 86 AA 3E DF" AfYw7Z6RzU9ZaGUloPhH3QpfA1AXWxnCGAXAwk3f6MoTx
 # General Usage's Output: decrypted text, encrypted cipher
